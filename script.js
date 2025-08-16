9419d8eecdc1fb9eb4da7f17f4ac5285a1453527
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-08-16 22:11:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+	let newStr = str.split("").reverse().join("");
+	
+	if(str === newStr){
+		console.log("true")
+	}else{
+		console.log(false)
+	}
 }
+
 module.exports = palindrome
